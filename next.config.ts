8659546498ed39b839c6f8f4d0804403a14b1bcc
keyboardbreaker <yujin_1991@hotmail.com>
2025-06-24 19:38:04 +0100

    adding next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
  
 const nextConfig: NextConfig = {
   /* config options here */
-
+  env: {
+    VITE_CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN: "CFPAT-Ss1igawBKlNnO_CifRknzQELLkeXQGrYzCM9yucXJkY",
+    VITE_CONTENTFUL_ACCESS_TOKEN: "fxMMw8RkqLAvtTvdvdaP-XDlehYg074JsymxQ4zNKKs",
+    VITE_CONTENTFUL_SPACE_ID: "ptlz289r9hfh",
+    VITE_CONTENTFUL_ENVIRONMENT: "master",
+  },
 };
  
 export default nextConfig;
